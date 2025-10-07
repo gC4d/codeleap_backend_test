79b08164843bc1f874b95475d0015748709300f3
gc4d <guilhermeacadorin@gmail.com>
2025-10-07 13:07:55 -0300

    Update database configuration to use DATABASE_URL provided by Heroku

--- src/config/database.py
+++ src/config/database.py
@@ -1,20 +1,34 @@
 from __future__ import annotations
 
 import logging
-from os import getenv
+import os
+from urllib.parse import urlparse
 
 logger = logging.getLogger(__name__)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': getenv("DB_NAME", default="postgres"),
-        'USER': getenv("DB_USER", default="postgres"),
-        'PASSWORD': getenv("DB_PASSWORD", default="postgres"),
-        'HOST': getenv("DB_HOST", default="localhost"),
-        'PORT': getenv("DB_PORT", default="5432"),
+# Use DATABASE_URL if provided (Heroku), otherwise use individual environment variables
+if 'DATABASE_URL' in os.environ:
+    url = urlparse(os.environ.get('DATABASE_URL'))
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': url.path[1:],
+            'USER': url.username,
+            'PASSWORD': url.password,
+            'HOST': url.hostname,
+            'PORT': url.port,
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv("DB_NAME", default="postgres"),
+            'USER': os.getenv("DB_USER", default="postgres"),
+            'PASSWORD': os.getenv("DB_PASSWORD", default="postgres"),
+            'HOST': os.getenv("DB_HOST", default="localhost"),
+            'PORT': os.getenv("DB_PORT", default="5432"),
+        }
+    }
