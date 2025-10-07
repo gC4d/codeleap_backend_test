d208bdbaef33ed81bc3f189254ba878d31c80d06
gc4d <guilhermeacadorin@gmail.com>
2025-10-07 14:43:25 -0300

    chore: remove static file handling and collection from Django config

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,8 +20,6 @@ echo "Database is ready!"
 echo "Running migrations..."
 python src/manage.py migrate --noinput
 
-python src/manage.py collectstatic
-
 echo "Checking memory usage..."
 free -h
 
--- src/config/static.py
+++ src/config/static.py
@@ -1,5 +1,4 @@
-from config.settings import BASE_DIR
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles" 
+
 MEDIA_URL = "/media/"
